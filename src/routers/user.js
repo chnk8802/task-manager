a7a7ebc503157588e296fa3cf3670625bfed2e52
chnk <119658399+chnk8802@users.noreply.github.com>
2023-07-03 16:47:05 +0530

    commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -62,7 +62,7 @@ router.get('/users/me', auth , async (req, res) => {
 });
 
 // Update a User
-router.patch('/users/update', auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update));
