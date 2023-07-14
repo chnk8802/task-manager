c6f4fe0e60a509f7143fa2d378183dd90d09865c
chnk <119658399+chnk8802@users.noreply.github.com>
2023-07-14 16:15:12 +0530

    m2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 require('./db/mongoose');
 // require('./db/mongoose')();
 const userRouter = require('./routers/user');
@@ -6,7 +7,7 @@ const taskRouter = require('./routers/task');
 
 // start Express server
 const app = express();
-
+app.use(cors());
 // To parse the incoming json data from POST request
 app.use(express.json());
 // Using Routes
