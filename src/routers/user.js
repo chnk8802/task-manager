9b3e3e08f0a06e0e034b875a148dc870ca003d1b
KumarNaveenKumar <naveenchaudahryyyy@gmail.com>
2023-07-15 21:58:43 +0530

    s

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,57 +7,57 @@ const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router();
 
 // Create User or SignUp
-router.post('/users', async (req, res) => {
+router.post('/users/signup', async (req, res) => {
     const user = new User(req.body);
 
-    try { 
+    try {
         await user.save();
         sendWelcomeEmail(user.email, user.name, user.password);
         const token = await user.generateAuthToken();
-        res.status(201).send({ message: 'User created successfully', user, token});
+        res.status(201).send({ message: 'User created successfully', user, token });
     } catch (e) {
-        res.status(400.).send({ error: 'Error creating user' });
+        res.status(400).send({ error: 'Error creating user' });
     }
 });
 
 // Login a User
 router.post('/users/login', async (req, res) => {
-        try {
-            const user = await User.findByCredentials(req.body.email, req.body.password);
-            const token = await user.generateAuthToken();
-            res.send({ Message: "Logged In Successfully!", user, token});
-        } catch (e) {
-            res.status(400).send();
-        }
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({ Message: `Logged In Successfully! You are Welcome ${user.name}`, user, token });
+    } catch (e) {
+        res.status(400).send({ error: 'Error logging in user' });
+    }
 });
 
 // Logout a User
 router.post('/users/logout', auth, async (req, res) => {
-        try {
-            req.user.tokens = req.user.tokens.filter((token) => {
-                return token.token !== req.token
-            });
-            await req.user.save();
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        });
+        await req.user.save();
 
-            res.send('User loggedout successfully!');
-        } catch (e) {
-            res.status(500).send(e);
-        }
+        res.send('User loggedout successfully!');
+    } catch (e) {
+        res.status(500).send({ error: 'Error logging out user' });
+    }
 });
 
 // Logout All Users
 router.post('/users/logoutAll', auth, async (req, res) => {
-        try {
-            req.user.tokens = [];
-            await req.user.save();
-            res.send('All users logged out!');
-        } catch (e) {
-            res.status(500).send(e);
-        }
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send('All users logged out!');
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 // User porfile
-router.get('/users/me', auth , async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
@@ -65,24 +65,24 @@ router.get('/users/me', auth , async (req, res) => {
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
-    const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update));
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' });
     }
 
-       try {
-            updates.forEach((update) => req.user[update] = req.body[update]);
-            await req.user.save();
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update]);
+        await req.user.save();
 
-            res.send(req.user);
+        res.send(req.user);
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
 // Delete a User
-router.delete('/users/me', auth, async(req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove();
         sendCancelationEmail(req.user.email, req.user.name);
@@ -96,9 +96,9 @@ router.delete('/users/me', auth, async(req, res) => {
 const upload = multer({
     limit: {
         FileSize: 1000000
-    }, 
-    fileFilter( req, file, cb) {
-        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
             return cb(new Error('Please upload an Image.'))
         }
         cb(undefined, true)
@@ -106,12 +106,12 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
     req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
+    res.status(400).send({ error: error.message })
 })
 
 // To delete an avatar
@@ -131,17 +131,17 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
 // To view an avatar
 router.get('/users/:id/avatar', async (req, res) => {
     try {
-            const user = await User.findById(req.params.id);
+        const user = await User.findById(req.params.id);
 
-            if (!user || !user.avatar) {
-                    throw new Error ();
-            }
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
 
-            res.set('Content-Type', 'image/png');
-            res.send(user.avatar);
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
     }
     catch (e) {
-            res.status(400).send();
+        res.status(400).send();
     }
 })
 
