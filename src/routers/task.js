803a19b84ff61b7a23fa3479d49915b135ce6570
chnk <119658399+chnk8802@users.noreply.github.com>
2023-07-19 17:55:03 +0530

    more

--- src/routers/task.js
+++ src/routers/task.js
@@ -8,7 +8,6 @@ router.post('/tasks', auth, async (req, res) => {
         ...req.body,
         owner: req.user._id
     })
-
     try {
         await task.save();
         res.status(201).send({task, message: "Task Added Successfully"});
@@ -52,10 +51,8 @@ router.get('/tasks', auth, async (req, res) => {
 // Read Task by id
 router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
-    console.log(_id, req.user._id);
     try {
         const task = await Task.findOne({ _id, owner: req.user._id});
-        console.log(task);
         if (!task) {
             return res.status(404).send({Error:"No Task(s) Found!!!!!"});
         }
@@ -69,7 +66,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 // Update a Task
 router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
-    const allowedUpdates = ['description', 'completed'];
+    const allowedUpdates = ['title', 'description', 'completed'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
     if (!isValidOperation) {
